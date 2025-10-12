80f0e387ae486a22ae389847f6e10a3923695769
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-10-12 18:40:48 +0100

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -4,13 +4,12 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCV1AhhjueVqy0dOmkt53uBkCNtsVC4lTc",
-  authDomain: "crestpoint-capital.firebaseapp.com",
-  projectId: "crestpoint-capital",
-  storageBucket: "crestpoint-capital.firebasestorage.app",
-  messagingSenderId: "204055812881",
-  appId: "1:204055812881:web:5a9d07c515c484229bcab0",
-  databaseURL: "https://crestpoint-capital-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyBSITcz0wWrZuVsSe0njOwB5K_iKuTwjxg",
+  authDomain: "crestpoint-bb103.firebaseapp.com",
+  projectId: "crestpoint-bb103",
+  storageBucket: "crestpoint-bb103.firebasestorage.app",
+  messagingSenderId: "249520798719",
+  appId: "1:249520798719:web:2d83f3cef937c9dc1405ca"
 };
 
 // Initialize Firebase
