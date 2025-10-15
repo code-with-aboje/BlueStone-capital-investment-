25bd418784b818f103e431b14df3b2cbcfbeb9c7
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-10-16 00:10:32 +0100

    Update firebaseConfig.js

--- admin/firebaseConfig.js
+++ admin/firebaseConfig.js
@@ -1,12 +1,12 @@
+// Firebase Configuration Object
 const firebaseConfig = {
-            apiKey: "AIzaSyBSITcz0wWrZuVsSe0njOwB5K_iKuTwjxg",
-            authDomain: "crestpoint-bb103.firebaseapp.com",
-            projectId: "crestpoint-bb103",
-            storageBucket: "crestpoint-bb103.firebasestorage.app",
-            messagingSenderId: "249520798719",
-            appId: "1:249520798719:web:2d83f3cef937c9dc1405ca"
-        };
+    apiKey: "AIzaSyBSITcz0wWrZuVsSe0njOwB5K_iKuTwjxg",
+    authDomain: "crestpoint-bb103.firebaseapp.com",
+    projectId: "crestpoint-bb103",
+    storageBucket: "crestpoint-bb103.firebasestorage.app",
+    messagingSenderId: "249520798719",
+    appId: "1:249520798719:web:2d83f3cef937c9dc1405ca"
+};
 
-        const app = initializeApp(firebaseConfig);
-        const auth = getAuth(app);
-        const db = getDatabase(app);
+// Export only the configuration
+export default firebaseConfig;
